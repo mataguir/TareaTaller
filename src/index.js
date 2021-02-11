7b161fb7db34c030c994b68e8377a7a8efc596aa
mataguir <mateinsis@gmail.com>
2021-02-10 23:27:41 -0300

    Reordené el head del html porque no cargaba bien el js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-//ons.ready(todoCargado);
+ons.ready(todoCargado);
 
 //document.querySelector('ons-input').blur() evento al salir del focus del input
 
 function todoCargado(){
-    
+    alert('test');
 }
